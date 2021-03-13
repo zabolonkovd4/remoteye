013f8219b28e2a07d71ea502fa7f3861ee764a9e
Dmitry Zabolonkov <dmitri.zabolonkov@ya.ru>
2021-03-14 00:49:06 +0300

    opencv simple example is ready

--- typeye_console/main.cpp
+++ typeye_console/main.cpp
@@ -1,10 +1,21 @@
-#include <QCoreApplication>
 #include <iostream>
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+
 using namespace std;
+using namespace cv;
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
-    cout << "create project..." << endl;
-    return a.exec();
+    Mat image;
+    image = imread("/home/diz/Downloads/800px-Lenna.png");
+    if ( !image.data )
+    {
+        printf("No image data \n");
+        return -1;
+    }
+    namedWindow("Display Image", WINDOW_AUTOSIZE);
+    imshow("Display Image", image);
+    waitKey(0);
+    return 0;
 }
