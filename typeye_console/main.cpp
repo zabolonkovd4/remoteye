3fba24461995a87e0c47f7c056d36a587e4de90b
Dmitry Zabolonkov <dmitri.zabolonkov@ya.ru>
2021-03-14 03:15:39 +0300

    Video capturing using opencv

--- typeye_console/main.cpp
+++ typeye_console/main.cpp
@@ -1,21 +1,69 @@
 #include <iostream>
-#include <stdio.h>
 #include <opencv2/opencv.hpp>
+#include <time.h>
 
 using namespace std;
 using namespace cv;
 
+double measureFps(VideoCapture &cap)
+{
+    double fps = cap.get(CAP_PROP_FPS);
+    cout << "Frames per second using cap.get(CAP_PROP_FPS) : " << fps << endl;
+    int num_frames = 120;
+    time_t start, end;
+    Mat frame;
+    cout << "Capturing " << num_frames << " frames" << endl;
+    time(&start);
+
+    for(int i = 0; i < num_frames; i++)
+        cap >> frame;
+
+    time(&end);
+    double seconds = difftime(end, start);
+    cout << "Time taken : " << seconds << " seconds" << endl;
+
+    fps = num_frames / seconds;
+    cout << "Estmated frames per second : " << fps << endl;
+    return fps;
+}
+
 int main(int argc, char *argv[])
 {
-    Mat image;
-    image = imread("/home/diz/Downloads/800px-Lenna.png");
-    if ( !image.data )
+    // Create a VideoCapture object and open the input file
+    // If the input is the web camera, pass 0 instead of the video file name
+    VideoCapture cap(0, cv::CAP_V4L2);
+
+    // Check if camera opened successfully
+    if(!cap.isOpened())
     {
-        printf("No image data \n");
-        return -1;
+      cout << "Error opening video stream or file" << endl;
+      return -1;
     }
-    namedWindow("Display Image", WINDOW_AUTOSIZE);
-    imshow("Display Image", image);
-    waitKey(0);
+
+    measureFps(cap);
+
+    while(1)
+    {
+      Mat frame;
+
+      // Capture frame-by-frame
+      cap >> frame;
+
+      // If the frame is empty, break immediately
+      if (frame.empty())
+        break;
+      // Display the resulting frame
+      imshow( "Frame", frame );
+      // Press  ESC on keyboard to exit
+      char c=(char)waitKey(25);
+      if(c==27)
+        break;
+    }
+
+    // When everything done, release the video capture object
+    cap.release();
+
+    // Closes all the frames
+    destroyAllWindows();
     return 0;
 }
